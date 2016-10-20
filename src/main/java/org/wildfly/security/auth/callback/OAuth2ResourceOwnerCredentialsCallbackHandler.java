a3bef1955ac9ea015295854feb0ccd78779fd220
Pedro Igor <pigor.craveiro@gmail.com>
2016-10-20 18:32:33 -0200

    [ELY-658] - OAuth2 Resource Owner Password Credentials Callback

---
+++ src/main/java/org/wildfly/security/auth/callback/OAuth2ResourceOwnerCredentialsCallbackHandler.java
@@ -0,0 +1,143 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2016 Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.wildfly.security.auth.callback;
+
+import static org.wildfly.common.Assert.checkNotNullParam;
+import static org.wildfly.security.util.HttpClient.HttpHeader.authorizationBasic;
+import static org.wildfly.security.util.HttpClient.HttpHeader.contentType;
+
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLContext;
+import javax.security.auth.callback.Callback;
+import javax.security.auth.callback.CallbackHandler;
+import javax.security.auth.callback.NameCallback;
+import javax.security.auth.callback.PasswordCallback;
+import javax.security.auth.callback.UnsupportedCallbackException;
+import java.io.IOException;
+import java.net.URL;
+
+import org.wildfly.security._private.ElytronMessages;
+import org.wildfly.security.credential.BearerTokenCredential;
+import org.wildfly.security.util.HttpClient;
+import org.wildfly.security.util.HttpClient.HttpRequest;
+
+/**
+ * <p>A {@link CallbackHandler} that support OAuth2 Resource Owner Password Credentials Grant Type.
+ *
+ * @author <a href="mailto:psilva@redhat.com">Pedro Igor</a>
+ */
+public final class OAuth2ResourceOwnerCredentialsCallbackHandler implements CallbackHandler {
+
+    private final CallbackHandler delegate;
+    private final URL tokenEndpointUrl;
+    private final SSLContext sslContext;
+    private final HostnameVerifier hostNameVerifier;
+    private final String clientId;
+    private final String clientSecret;
+    private final String scopes;
+
+    /**
+     * Creates a new instance.
+     *
+     * @param delegate a {@link CallbackHandler} that will be used to obtain the username and password
+     * @param tokenEndpointUrl a {@link URL} that references the OAuth2 Token Endpoint
+     * @param clientId the client id
+     * @param clientSecret the client secret
+     */
+    public OAuth2ResourceOwnerCredentialsCallbackHandler(CallbackHandler delegate, URL tokenEndpointUrl, String clientId, String clientSecret) {
+        this(delegate, tokenEndpointUrl, null, null, clientId, clientSecret, null);
+    }
+
+    /**
+     * Creates a new instance.
+     *
+     * @param delegate a {@link CallbackHandler} that will be used to obtain the username and password
+     * @param tokenEndpointUrl a {@link URL} that references the OAuth2 Token Endpoint
+     * @param sslContext a {@link SSLContext} in case the <code>tokenEndpointUrl</code> is configured with SSL/TLS
+     * @param hostNameVerifier a {@link HostnameVerifier} in case the <code>tokenEndpointUrl</code> is configured with SSL/TLS
+     * @param clientId the client id
+     * @param clientSecret the client secret
+     */
+    public OAuth2ResourceOwnerCredentialsCallbackHandler(CallbackHandler delegate, URL tokenEndpointUrl, SSLContext sslContext, HostnameVerifier hostNameVerifier, String clientId, String clientSecret) {
+        this(delegate, tokenEndpointUrl, sslContext, hostNameVerifier, clientId, clientSecret, null);
+    }
+
+    /**
+     * Creates a new instance.
+     *
+     * @param delegate a {@link CallbackHandler} that will be used to obtain the username and password
+     * @param tokenEndpointUrl a {@link URL} that references the OAuth2 Token Endpoint
+     * @param sslContext a {@link SSLContext} in case the <code>tokenEndpointUrl</code> is configured with SSL/TLS
+     * @param hostNameVerifier a {@link HostnameVerifier} in case the <code>tokenEndpointUrl</code> is configured with SSL/TLS
+     * @param clientId the client id
+     * @param clientSecret the client secret
+     * @param scopes a string with the scope of the access request
+     */
+    public OAuth2ResourceOwnerCredentialsCallbackHandler(CallbackHandler delegate, URL tokenEndpointUrl, SSLContext sslContext, HostnameVerifier hostNameVerifier, String clientId, String clientSecret, String scopes) {
+        this.delegate = checkNotNullParam("delegate", delegate);
+        this.tokenEndpointUrl = checkNotNullParam("tokenEndpointUrl", tokenEndpointUrl);
+        this.sslContext = sslContext;
+        this.hostNameVerifier = hostNameVerifier;
+        this.clientId = checkNotNullParam("clientId", clientId);
+        this.clientSecret = checkNotNullParam("clientSecret", clientSecret);
+        this.scopes = scopes;
+    }
+
+    @Override
+    public void handle(Callback[] callbacks) throws IOException, UnsupportedCallbackException {
+        for (Callback callback : callbacks) {
+            if (callback instanceof CredentialCallback) {
+                CredentialCallback credentialCallback = (CredentialCallback) callback;
+                NameCallback nameCallback = new NameCallback("Username:");
+                PasswordCallback passwordCallback = new PasswordCallback("Password:", false);
+
+                delegate.handle(new Callback[] {nameCallback, passwordCallback});
+
+                String name = checkNotNullParam("Username", nameCallback.getName());
+                char[] password = checkNotNullParam("Password", passwordCallback.getPassword());
+
+                HttpRequest request = HttpClient.builder().sslContext(sslContext).hostNameVerifier(hostNameVerifier).build()
+                        .post(tokenEndpointUrl)
+                            .header(contentType("application/x-www-form-urlencoded"))
+                            .header(authorizationBasic(clientId, clientSecret))
+                            .param("grant_type", "password")
+                            .param("username", name)
+                            .param("password", String.valueOf(password));
+
+                if (scopes != null) {
+                    request.param("scope", scopes);
+                }
+
+                BearerTokenCredential credential = request
+                            .execute((inputStream, throwable) -> {
+                                if (throwable != null) {
+                                    throw ElytronMessages.log.httpClientUnexpectedResponseFromServer(throwable);
+                                }
+                                JsonObject jsonObject = Json.createReader(inputStream).readObject();
+                                String accessToken = jsonObject.getString("access_token");
+                                return new BearerTokenCredential(accessToken);
+                            });
+
+                credentialCallback.setCredential(credential);
+            }
+        }
+    }
+}
